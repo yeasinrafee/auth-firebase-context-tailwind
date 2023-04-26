1469fbfa37a8da1188e36e952561a8340be05027
yeasinrafee <mdyeasinrafi7@gmail.com>
2023-04-26 08:39:02 +0600

    Showing email in different route and log out implemented

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,18 @@
-import React from "react";
+import { useContext } from "react";
 import { Link } from "react-router-dom";
+import { AuthContext } from "../provider/AuthProvider";
 
 const Header = () => {
+  const { user, logOut } = useContext(AuthContext);
+
+  const handleLogOut = () => {
+    logOut()
+      .then((result) => {
+        const loggedOut = result.user;
+        console.log("User Logged out", loggedOut);
+      })
+      .catch((error) => console.log(error.message));
+  };
   return (
     <div className="navbar bg-neutral text-neutral-content">
       <a className="btn btn-ghost normal-case text-xl" href="/">
@@ -16,6 +27,16 @@ const Header = () => {
       <Link className="btn btn-ghost normal-case text-xl" to="register">
         Register
       </Link>
+      {user ? (
+        <>
+          <>{user.email}</>{" "}
+          <button onClick={handleLogOut} className="ml-5 font-bold">
+            Sign Out
+          </button>
+        </>
+      ) : (
+        <Link to="/login">Log in</Link>
+      )}
     </div>
   );
 };
--- src/provider/AuthProvider.jsx
+++ src/provider/AuthProvider.jsx
@@ -1,8 +1,10 @@
-import React, { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 import {
   createUserWithEmailAndPassword,
   getAuth,
+  onAuthStateChanged,
   signInWithEmailAndPassword,
+  signOut,
 } from "firebase/auth";
 import app from "../firebase/firebase.config";
 export const AuthContext = createContext(null);
@@ -20,10 +22,26 @@ const AuthProvider = ({ children }) => {
     return signInWithEmailAndPassword(auth, email, password);
   };
 
+  const logOut = () => {
+    return signOut(auth);
+  };
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      console.log("Auth state change", currentUser);
+      setUser(currentUser);
+    });
+
+    return () => {
+      unsubscribe();
+    };
+  }, []);
+
   const authInfo = {
     user,
     createUser,
     signIn,
+    logOut,
   };
 
   return (
